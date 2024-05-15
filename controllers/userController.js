b3c4a04a4905dc6f1b05296db4637183a44684ad
Zu9zwan9 <46656676+Zu9zwan9@users.noreply.github.com>
2024-05-15 19:21:39 -0300

    upd firebase

--- controllers/userController.js
+++ controllers/userController.js
@@ -46,7 +46,7 @@ exports.user_get_by_id = asyncHandler(async (req, res, next) => {
 
         res.status(200).json(result);
     } else {
-        res.status(422).json({mesasge: "error", id: req.params.id});
+        res.status(422).json({message: "error", id: req.params.id});
     }
 });
 
@@ -83,7 +83,7 @@ exports.user_edit_by_id = asyncHandler(async (req, res, next) => {
 
         res.status(200).json(user);
     } else {
-        res.status(422).json({mesasge: "error", id: req.params.id});
+        res.status(422).json({message: "error", id: req.params.id});
     }
 });
 
